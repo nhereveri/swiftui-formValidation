2f1f63dd8a393da37083c6805b86b35ed1902402
Nelson Hereveri <nelson@hereveri.cl>
2023-03-08 19:58:54 -0300

    Default project

---
+++ FormValidation/FormValidationApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct FormValidationApp: App {
+  var body: some Scene {
+    WindowGroup {
+      RegisterUserView()
+    }
+  }
+}
---
+++ FormValidation/RegisterUserView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct RegisterUserView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundColor(Color("accent"))
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+struct RegisterUserView_Previews: PreviewProvider {
+  static var previews: some View {
+    RegisterUserView()
+  }
+}
